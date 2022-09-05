dbc74b4beb4db343eea4705e077d5a2215f979ad
hankaibo <hankaibo@thtf.com.cn>
2022-09-05 20:33:59 +0800

    fix: 解决一个大小写问题

--- packages/layout/index.js
+++ packages/layout/index.js
@@ -1,4 +1,4 @@
-import Layout from './src/Layout.vue';
+import Layout from './src/index.vue';
 import ParentView from './src/ParentView';
 import InnerLink from './src/InnerLink';
 
--- packages/layout/src/utils.js
+++ packages/layout/src/utils.js
@@ -1,4 +1,4 @@
-import Layout from './Layout';
+import Layout from './index';
 import ParentView from './ParentView';
 import InnerLink from './InnerLink';
 
